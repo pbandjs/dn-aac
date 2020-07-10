bc926cd46cae3c3e70b8ba3ddd9fb2a1b681ca26
Kushal Shah <kushalshah8@gmail.com>
2020-07-11 02:46:19 +0530

    Updates

---
+++ precache-manifest.cee6aba9e3b422e869043f85512c736a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af854df6de6b76aaa77b3217d9081e3d",
+    "url": "/dn-aac/index.html"
+  },
+  {
+    "revision": "3a4b566c619df31bb214",
+    "url": "/dn-aac/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "277b9ee947e5691604aa",
+    "url": "/dn-aac/static/js/2.2a1707bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dn-aac/static/js/2.2a1707bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4b566c619df31bb214",
+    "url": "/dn-aac/static/js/main.f9fe670e.chunk.js"
+  },
+  {
+    "revision": "958b6c420ad072080f38",
+    "url": "/dn-aac/static/js/runtime-main.82337422.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dn-aac/static/media/logo.5d5d9eef.svg"
+  }
+]);
