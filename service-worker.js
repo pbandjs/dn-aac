0703e166ff5698b72a45df3b3e5de06af420700d
Kushal Shah <kushalshah8@gmail.com>
2020-08-08 13:19:28 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dn-aac/precache-manifest.dca7cb814e58bc328138fa25f28d4681.js"
+  "/dn-aac/precache-manifest.2fa37ebe6f968aa3e9339a57da5da2d3.js"
 );
 
 self.addEventListener('message', (event) => {
